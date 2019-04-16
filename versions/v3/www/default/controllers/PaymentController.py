482ef9160e2ce84807421418fd4ecf525d439658
Samuel Colak <scolak@MBP2017.local>
2019-04-16 13:06:40 +0200

    update to payment controller

--- versions/v3/www/default/controllers/PaymentController.py
+++ versions/v3/www/default/controllers/PaymentController.py
@@ -230,13 +230,12 @@ class PaymentController(XscroController):
 					transactionid = paramsin_.transactionid,
 					chainid = paramsin_.chainid,
 					recipient = {"walletid":paramsin_.recipientwallet, "displayas":paramsin_.recipientdisplay},
-					sender = {"walletid":paramsin_.default("sender", "")},
 					description = paramsin_.description,
 					amount = float(paramsin_.amount),
 					token = paramsin_.currency,
 					callbacks = {"success":paramsin_.callbacksuccess, "fail":paramsin_.callbackfailure, "cancel":paramsin_.callbackcancel}
 					)
-
+			
 			else:
 			
 				payment_ = extdict({"transactionid":paramsin_.transactionid, "chainid":paramsin_.chainid,
